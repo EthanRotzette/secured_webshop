4d3d6148b77c9978eda4faf83f651a8ea443656a
EthanRotzette <167179837+EthanRotzette@users.noreply.github.com>
2025-01-20 13:58:54 +0100

    feat(Secured_webshop) lien avec https

--- app/server.js
+++ app/server.js
@@ -1,13 +1,20 @@
 const express = require("express");
-
+const fs = require('fs');
+const https = require("https")
 
 const app = express();
-const userRoute = require('./routes/User');
+const userRoute = require('./routes/user');
 app.use('/user', userRoute);
 
 //Ajouter la licence
+// Charger les clés SSL
+const privateKey = fs.readFileSync('../keys/privkey.key', 'utf8');
+const certificate = fs.readFileSync('../keys/certificate.crt', 'utf8');
+const credentials = { key: privateKey, cert: certificate };
+// Créer le serveur HTTPS
+const httpsServer = https.createServer(credentials, app);
 
 // Démarrage du serveur
-app.listen(443, () => {
-    console.log('Server running on port 443');
+httpsServer.listen(443, () => {
+    console.log('Server running on port 443 with https');
 });
